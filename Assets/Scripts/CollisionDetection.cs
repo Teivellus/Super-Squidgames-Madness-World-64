b8b9df58e46719cc94d4ff8d3a4779cbee228c00
Jhaydun Dinan <jhaydundinan@hotmail.com>
2023-04-05 05:02:32 +1000

    Removed a poorly implemented collision detector from the objects which was making the game crash.

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class CollisionDetection : MonoBehaviour
 {
-    public int _hatred;
     // Start is called before the first frame update
     void Start()
     {
